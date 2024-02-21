e20f413d1b18f18f0ccfcae538d0b55237dd7ba3
Milind Kumar V <milind.blaze9@gmail.com>
2024-02-21 17:36:37 -0600

    Add new server, add new client and add script to compute meand and standard deviation of resulting csv

--- mp1/src/http_client.cpp
+++ mp1/src/http_client.cpp
@@ -21,204 +21,134 @@
 
 
 using namespace std;
-
-#define PORT "3490" // the port client will be connecting to 
+#define PORT "9000" // the port client will be connecting to 
 
 #define MAXDATASIZE 4096 // max number of bytes we can get at once 
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 int main(int argc, char *argv[])
 {
-	int sockfd, numbytes;  
-	char buf[MAXDATASIZE];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
-    string addr, protocol, host, port, path;
+    int sockfd, numbytes;  
+    char buf[MAXDATASIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
 	
-	FILE *client_timestamp_fp;
-
+	int total_bytes = 0;
 	const int MAX_TIMESTAMP_COUNT = 100000; // Define the maximum number of timestamps you want to store
 	long long timestamp_array[MAX_TIMESTAMP_COUNT];
 	int timestamp_count = 0;
 
-	int sleep_time = 10000; // sleep time in microseconds
+    if (argc != 6) {
+        fprintf(stderr,"usage: client hostname n_iterations timestamp_file message_size sleep_time\n");
+        exit(1);
+    }
+
+	int n_iterations = atoi(argv[2]);
+	char *client_file_name = argv[3];
+	string request = argv[4];
+	int sleep_time = atoi(argv[5]);
+	int datasize = stoi(request);
+	int data_tracking = datasize;
 
 
-	if (argc != 5) {
-	    fprintf(stderr,"usage: client hostname filename_latency_timestamp filesize n_iterations\n");
-	    exit(1);
-	}
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-    
-    addr = argv[1];
-	char *client_file_name = argv[2];
-	long filesize = atoi(argv[3]);
-	int n_iterations = atoi(argv[4]);
-	long timestamp_filesize = filesize;
-	client_timestamp_fp = fopen(client_file_name, "w");
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
 
-    protocol = addr.substr(0, addr.find("//") - 1);
-	addr = addr.substr(addr.find("//") + 2);
-	if (addr.find('/') == addr.npos) path = "/";
-	else path = addr.substr(addr.find('/'));
-	host = addr.substr(0, addr.find('/'));
-	if (host.find(':') != host.npos) {
-		port = host.substr(host.find(':') + 1);
-		host = host.substr(0, host.find(':'));
-	}
-	else port = "80";
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
 
-	if ((rv = getaddrinfo(host.data(), port.data(), &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
 
-	// loop through all the results and connect to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("client: socket");
-			continue;
-		}
+        break;
+    }
 
-		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("client: connect");
-			continue;
-		}
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
 
-		break;
-	}
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+    printf("client: connecting to %s\n", s);
 
-	if (p == NULL) {
-		fprintf(stderr, "client: failed to connect\n");
-		return 2;
-	}
+    freeaddrinfo(servinfo); // all done with this structure
 
-	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-			s, sizeof s);
-	
-	// TODO: Remove this
-	printf("client: connecting to %s\n", s);
+	for (int i = 0; i < n_iterations; i++) {
 
-	freeaddrinfo(servinfo); // all done with this structure
-	
-	// TODO: Remove this
-	cout << host << ' ' << port << ' ' << path << endl;
-	
-    string request = "GET " + path + " HTTP/1.1\r\n" + "User-Agent: Wget/1.12(linux-gnu)\r\n" +
-	  				 "Host: " + host + ":" + port + "\r\n" + "Connection: Keep-Alive\r\n\r\n";
-
-
-	// ofstream out;
-	// out.open("output", ios::binary);
-	long total_bytes = 0;
-
-	for (int j = 0; j < n_iterations; ++j) {
-		usleep(sleep_time);
-		struct timeval current_time;
-		long long microseconds;
-
-
-		// gettimeofday(&current_time, NULL);
-		// microseconds = (long long)current_time.tv_sec * 1000000 + current_time.tv_usec;
-		// timestamp_array[timestamp_count++] = microseconds;
-
-		// Start timestamp
+		
+		usleep(sleep_time); // 1ms
 		auto start = std::chrono::high_resolution_clock::now();
-
 		send(sockfd, request.c_str(), request.size(), 0);
+
 		
 
-
-		bool header = true;
-		
-		// printf("total bytes: %ld", total_bytes);
-		
-
-
-		while (true) {
-			memset(buf, '\0', MAXDATASIZE);
-			numbytes = recv(sockfd, buf, MAXDATASIZE, 0);
-			buf[numbytes] = '\0';
+		while (true){
+			memset(buf, '\0', MAXDATASIZE); // TODO: dubious
+			if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+				perror("recv");
+				exit(1);
+			}
 			if (numbytes > 0) {
-				// cout << numbytes << endl;
 				total_bytes += numbytes;
-				// if (header) {
-				// 	// cout << buf << endl;
-				// 	char* head = strstr(buf, "\r\n\r\n");
-				// 	// cout << head << endl;
-				// 	if (head != NULL){
-				// 		head+=4;
-				// 		header = false;
-				// 	}
-				// 	// out.write(head, numbytes - 19);
-				// 	// printf("strlen(head): %ld", strlen(head) );
-				// 	// printf("numbytes head %d", numbytes);
-				// } 
-				// else out.write(buf, sizeof(char) * numbytes);
-
-				if (total_bytes - 19 >= timestamp_filesize) {
-					// "TIMESTAMP" is in the received message
-					// printf("printing timestamp\n");
-					// gettimeofday(&current_time, NULL);
-					// microseconds = (long long)current_time.tv_sec * 1000000 + current_time.tv_usec;
-					// End timestamp
+				if (total_bytes == data_tracking) {
+					// printf("total_bytes: %d\n", total_bytes);
+					data_tracking += datasize;
 					auto end = std::chrono::high_resolution_clock::now();
-					timestamp_array[timestamp_count++] = std::chrono::duration<double, std::micro>(end - start).count();
-					timestamp_filesize += filesize;
-					// printf("total bytes: %ld \n", total_bytes);
-					// printf("j: %d\n \n", j);
-					fflush(stdout);
+					// printf("%ld\n", std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
+					timestamp_array[timestamp_count++] =
+						std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 					break;
-				} 
-			} 
-			else break;
+				}
+			} else {
+				break;
+			}
 		}
+		
+
+	buf[numbytes] = '\0';
+
+    // printf("client: received '%s'\n",buf);
+
 	}
 
-	// gettimeofday(&current_time, NULL);
-	// microseconds = (long long)current_time.tv_sec * 1000000 + current_time.tv_usec;
-	// printf("%lld\n", microseconds);
-	
-
-	for (int i = 0; i < timestamp_count; ++i) {
-		// if i is even, then print "start time" before the timestamp and if i is odd, then print 
-		// "end time" before the timestamp
-		// if (i % 2 == 0) fprintf(client_timestamp_fp, "start time: ");
-		// else fprintf(client_timestamp_fp, "end time: ");
+	// Write the timestamps to a file
+    FILE *client_timestamp_fp;
+	client_timestamp_fp = fopen(client_file_name, "w");
+    for (int i = 0; i < timestamp_count; ++i) {
 		fprintf(client_timestamp_fp, "%lld\n", timestamp_array[i]);
 	}
 	fclose(client_timestamp_fp);
-	
-	printf("total bytes: %ld", total_bytes);
 
-	// if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-	//     perror("recv");
-	//     exit(1);
-	// }
-	close(sockfd);
+    close(sockfd);
 
-	// buf[numbytes] = '\0';
+	printf("total_bytes received: %d, total_bytes_expected = %d, Agree: %s \n", total_bytes, 
+		n_iterations*datasize, total_bytes == n_iterations*datasize ? "true" : "false");
 
-	// printf("client: received '%s'\n",buf);
-
-	// out.close(); 
-
-	return 0;
+    return 0;
 }
-
--- mp1/src/http_server.cpp
+++ mp1/src/http_server.cpp
@@ -14,130 +14,118 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include <iostream>
-#include <istream>
-#include <time.h>
-#include <netinet/tcp.h>
+#include <netinet/tcp.h> 
 
-using namespace std;
+#define PORT "9000"  // the port users will be connecting to
+#define MAXDATASIZE 4096
 
-
-#define PORT "3490"  // the port users will be connecting to
-
-#define BACKLOG 10	 // how many pending connections queue will hold
-#define MAXDATASIZE 4096 // max number of bytes we can get at once 
+#define BACKLOG 10   // how many pending connections queue will hold
 
 void sigchld_handler(int s)
-{	
-    s++;
-	while(waitpid(-1, NULL, WNOHANG) > 0);
+{
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
 }
+
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[])
+int main(void)
 {
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
 	int numbytes;
-	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr; // connector's address information
-	socklen_t sin_size;
-	struct sigaction sa;
-	int yes=1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
+	int total_bytes = 0;
+	
 
 
 
-
-	if (argc != 2) {
-	    fprintf(stderr,"usage: server port\n");
-	    exit(1);
-	}
-
-
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
-
-
-	if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("server: socket");
-			continue;
-		}
-
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-				sizeof(int)) == -1) {
-			perror("setsockopt");
-			exit(1);
-		}
-
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("server: bind");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL)  {
-		fprintf(stderr, "server: failed to bind\n");
-		return 2;
-	}
-
-	freeaddrinfo(servinfo); // all done with this structure
-
-	if (listen(sockfd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
-
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
-
-	printf("server: waiting for connections...\n");
-
-	char buf[MAXDATASIZE];
-	FILE *fp;
-	int SIZE = 1048575; // 1MB
+	int SIZE = 2048575; // 10MB
 	char data_in_memory[SIZE];
 	for (int i = 0; i < SIZE; i++) {
         data_in_memory[i] = 'a'; // Example filler data
     }
 
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
 
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
-	while(1) {  // main accept() loop
-		sin_size = sizeof their_addr;
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1) {
-			perror("accept");
-			continue;
-		}
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
 
 		int flag = 1; // Flag to enable TCP_NODELAY
 		int result;
@@ -150,96 +138,69 @@ int main(int argc, char *argv[])
 			exit(EXIT_FAILURE);
 		}
 
-		inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof s);
-		printf("server: got connection from %s\n", s);
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server: got connection from %s\n", s);
 
-		// if (!fork()) { // this is the child process
-		close(sockfd); // child doesn't need the listener
+        // if (!fork()) { // this is the child process
+        //     close(sockfd); // child doesn't need the listener
+        //     if (send(new_fd, "Hello, world!", 13, 0) == -1)
+        //         perror("send");
+        //     close(new_fd);
+        //     exit(0);
+        // }
 
-		long total_bytes = 0;
-
+		close(sockfd);
+		char buf[MAXDATASIZE];
+		
 		while (true){
+			numbytes = recv(new_fd, buf, MAXDATASIZE-1, 0);
 
-			int recv_value = recv(new_fd, buf, MAXDATASIZE, 0);
 			int offset = 0;
-			if (recv_value == 0) break;
 
-			string new_buf(buf);
-
-			// cout << new_buf << endl;
-			if (new_buf.find_first_of("GET ") == 0) {
-				new_buf = new_buf.substr(4);
-				// cout << new_buf << endl;
-				if (new_buf.find(" HTTP") != new_buf.npos) {
-					string path = new_buf.substr(1, new_buf.find(" HTTP")-1);
-					// cout << path << endl;
-					fp = fopen(path.data(), "rb");
-					if (fp != NULL) new_buf = "HTTP/1.1 200 OK\r\n\r\n";
-					else new_buf = "HTTP/1.1 404 Not Found\r\n\r\n";
-				}
-				else new_buf = "HTTP/1.1 400 Bad Request\r\n\r\n";
-			}
-			else new_buf = "HTTP/1.1 400 Bad Request\r\n\r\n";
-
-			if (send(new_fd, new_buf.data(), new_buf.size(), 0) == -1) {
-				perror("send");
-				exit(1);
-			}		
-
-				
-
-			memset(buf, '\0', MAXDATASIZE);
-
-			// gettimeofday(&current_time, NULL);
-			// microseconds = (long long)current_time.tv_sec * 1000000 + current_time.tv_usec;
-			// printf("%lld\n", microseconds);
-			
-			// while (true) {
-				
-			// 	numbytes = fread(buf, sizeof(char), MAXDATASIZE, fp);
-			// 	if (numbytes > 0) {
-			// 		if (send(new_fd, buf, numbytes, 0) == -1) {
-			// 			perror("send");
-			// 			exit(1);
-			// 		}	
-			// 		memset(buf, '\0', MAXDATASIZE);
-			// 		total_bytes += numbytes;
-			// 	}
-			// 	else break;
-			// }
-
-			while (offset < SIZE) {
-			// Calculate the number of bytes to send in this chunk
-			int bytes_to_send = (SIZE - offset > MAXDATASIZE) ? MAXDATASIZE : (SIZE - offset);
-			
-			// Send the data in chunks of MAXDATASIZE
-			numbytes = send(new_fd, data_in_memory + offset, bytes_to_send, 0);
 			if (numbytes == -1) {
-				perror("send");
+				perror("recv");
 				exit(1);
+			} else if (numbytes == 0) {
+				printf("server: connection closed\n");
+				break;
 			}
 
+			// printf("server: received '%s'\n", buf);
+
+			int size = atoi(buf);
+			// printf("size: %d\n", size);
+
+			while (offset < size) {
+				// Calculate the number of bytes to send in this chunk
+				int bytes_to_send = (size - offset > MAXDATASIZE) ? MAXDATASIZE : (size - offset);
+				
+				// Send the data in chunks of MAXDATASIZE
+				numbytes = send(new_fd, data_in_memory + offset, bytes_to_send, 0);
+				if (numbytes == -1) {
+					perror("send");
+					exit(1);
+				}
+
+				
+				offset += numbytes; // Move the offset forward
+				total_bytes += numbytes; // Accumulate the total bytes sent
+			}
+
+
 			
-			offset += numbytes; // Move the offset forward
-			total_bytes += numbytes; // Accumulate the total bytes sent
+			// if (send(new_fd, "Hello, world!", 13, 0) == -1)
+            //     perror("send");
+			
 		}
 
-			fclose(fp);
-		}
 
-
-		// if (send(new_fd, "Hello, world!", 13, 0) == -1)
-		// 	perror("send");
-		close(new_fd);
-		printf("total bytes: %ld", total_bytes);
-		// 	exit(0);
-		// }
-		// close(new_fd);  // parent doesn't need this
+        close(new_fd);  // parent doesn't need this
 		break;
-	}
+    }
 
-	return 0;
+	printf("server: sent %d bytes\n", total_bytes);
+
+    return 0;
 }
-
